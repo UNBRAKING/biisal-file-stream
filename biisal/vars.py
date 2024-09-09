f99b3c0570db97b2cd16adfd009c4ebbd46c3b5f
UNBRAKING <code.pbail@gmail.com>
2024-09-09 19:44:10 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -39,7 +39,7 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://6ktb3agl48:<db_password>@cluster0.6hpis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://l5jh9vmvq6:knuiVOOPegNaJTbq@cluster0.ij9qa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Pbail_Squad')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1002215739707")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
